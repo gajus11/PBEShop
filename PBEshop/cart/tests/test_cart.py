dd7ed35cf9f49b2d3495f68c7022e252f043a9ae
lgajownik <gajus11@gmail.com>
2016-06-02 12:42:18 +0200

    Add another tests to cart

--- PBEshop/cart/tests/test_cart.py
+++ PBEshop/cart/tests/test_cart.py
@@ -48,11 +48,27 @@ class CartTest(TestCase):
         cart.add(self.product, quantity=2, update_quantity=False)
         self.assertEqual(cart.cart[str(self.product.id)]['quantity'], 3)
 
-    def test_remove(self):
+    def test_remove_element_exist(self):
         cart = Cart(self.request)
         cart.add(self.product)
         cart.remove(self.product)
         self.assertEqual(cart.cart, {})
+
+    def test_remove_element_not_exist(self):
+        cart = Cart(self.request)
+        cart.add(self.product)
+        product = Product.objects.create(category=self.category,
+                                         name='name',
+                                         slug='slug',
+                                         description='desc',
+                                         price=Decimal(2),
+                                         stock=2,
+                                         available=True)
+        cart.remove(product)
+        self.assertNotEqual(cart.cart, {})
+        with self.assertRaises(KeyError):
+            cart.cart[str(product.id)]
+
 
     def test_clear(self):
         cart = Cart(self.request)
@@ -74,3 +90,41 @@ class CartTest(TestCase):
         cart.add(product, quantity=2)
         total_price = self.product.price + product.price*2
         self.assertEqual(cart.get_total_price(), Decimal(total_price))
+
+    def test_iteration(self):
+        product = Product.objects.create(category=self.category,
+                                         name='name',
+                                         slug='slug',
+                                         description='desc',
+                                         price=Decimal(2),
+                                         stock=2,
+                                         available=True)
+        cart = Cart(self.request)
+        cart.add(self.product)
+        cart.add(product, quantity=2)
+
+        i = 0
+
+        for prod in cart:
+            if prod['product'] == self.product:
+                self.assertEqual(prod['price'], self.product.price)
+            elif prod['product'] == product:
+                self.assertEqual(prod['price'], product.price)
+            else:
+                self.fail()
+            i += 1
+
+        self.assertEqual(i, 2)
+
+    def test_len(self):
+        product = Product.objects.create(category=self.category,
+                                         name='name',
+                                         slug='slug',
+                                         description='desc',
+                                         price=Decimal(2),
+                                         stock=2,
+                                         available=True)
+        cart = Cart(self.request)
+        cart.add(self.product)
+        cart.add(product, quantity=2)
+        self.assertEqual(cart.__len__(), 3)
