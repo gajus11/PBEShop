0f916b6e4f485f3ca7d3660b40e7271e7615f746
lgajownik <gajus11@gmail.com>
2016-06-02 14:38:38 +0200

    Fix the urlpatterns issue

--- PBEshop/cart/urls.py
+++ PBEshop/cart/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from . import views
 
-urlpatters = [
+urlpatterns = [
     url(r'^$', views.cart_detail, name='cart_detail'),
     url(r'^add/(?P<product_id>\d+)/$',
         views.cart_add,
